3d9e8f609749b843017dade637e9cb014c8daaea
LEGACY21\tkngu <tknguyen2111@gmail.com>
2026-04-05 18:06:36 -0700

    Fix config bug

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,5 +1,9 @@
-import "dotenv/config";
+import dotenv from "dotenv";
 import { z } from "zod";
+
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
 
 const envSchema = z.object({
   NODE_ENV: z.enum(["development", "production", "test"]).optional(),
